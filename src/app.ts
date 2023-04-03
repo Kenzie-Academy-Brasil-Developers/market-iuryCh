33024a8bc5f73586a6311342f2f1655fbf311ec0
iurych <iury.vibe@gmail.com>
2023-04-03 02:09:49 -0300

    correct some bugs

--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,11 @@ import { ensureProductExist, filterProductByName } from './middlewares';
 const app: Application = express();
 app.use(json());
 
-//create products
+
 app.post('/products', filterProductByName, createProduct);
-//list all products
 app.get('/products', readProducts);
-//list especif product
 app.get('/products/:id', ensureProductExist);
-//update product
 app.patch('/products/:id', ensureProductExist, filterProductByName);
-//delete product
 app.delete('/products/:id', ensureProductExist, eraseProduct);
 
 const port: number = 3000;
